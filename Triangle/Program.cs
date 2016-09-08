8cecc372ebb46550cb753bef9ece9200dc3ea3b6
apolekhin <Cr3dl1n3R@gmail.com>
2016-09-08 14:37:10 +0300

    initial

--- Triangle/Program.cs
+++ Triangle/Program.cs
@@ -14,8 +14,8 @@ namespace Triangle
             string path = GetPath();
             try
             {
-                List<List<int>> arr = TxtToList(path);
-                Triangle.CalculatePath(arr);
+                List<List<int>> triangle = TxtToList(path);
+                Triangle.CalculatePath(triangle);
             }
             catch (Exception ex)
             {
@@ -26,48 +26,44 @@ namespace Triangle
 
         public class Triangle
         {
+            
+            /*----------------------------------
+             * Goes up from penultimate line, 
+             *replacing current cell with the 
+             *max sum with its adjacent cells */
+
             public static void CalculatePath(List<List<int>> arr)
             {
-                /*Goes up from penultimate line, 
-                 *replacing current cell with the max sum with its adjacent cells */
                 int height = arr.Count;
 
                 if (IsIsosceles(arr) && height > 1)
                 {
                     for (int i = height - 2; i >= 0; i--)
-                    {
                         for (int j = 0; j <= i; j++)
-                        {
                             arr[i][j] += System.Math.Max(arr[i + 1][j], arr[i + 1][j + 1]);
-                        }
-                    }
                     Console.WriteLine("\nThe max sum path is: " + arr[0][0]);
                 }
                 else if (height == 1)
-                {
                     Console.WriteLine("\nTriangle has only one number. The max sum path is: " + arr[0][0]);
-                }
                 else
-                {
                     Console.WriteLine("The triangle is not suitable for this test");
-                }
             }
+
+            /*-------------------------------------------------------
+             * Checks, whether the triangle has appropriate shape.
+             *Line number must be equal to the ammount of values in it.*/
 
             public static bool IsIsosceles(List<List<int>> arr)
             {
-                /*Checks, whether the triangle has appropriate shape.
-                 *Line number must be equal to the ammount of values in it.*/
                 int height = arr.Count;
                 bool flag = true;
 
                 for (int i = 0; i < height; i++)
-                {
                     if (arr[i].Count != i + 1)
                     {
                         flag = false;
                         break;
                     }
-                }
                 return flag;
             }
         }
@@ -81,15 +77,16 @@ namespace Triangle
                 Console.WriteLine("Enter a path to txt file:");
                 path = Console.ReadLine();
             }
-
             while (!System.IO.File.Exists(path));
 
             return path;
         }
 
+        /*--------------------------------------------
+         * Reads txt file and populates list from it*/
+
         public static List<List<int>> TxtToList( string path )
         {
-            /*Reads txt file and populates list from it*/
             Console.WriteLine("Reading from: " + path);
             string[] input = System.IO.File.ReadAllLines(path);
             List<List<int>> triangle = new List<List<int>>();
@@ -100,7 +97,6 @@ namespace Triangle
                     int[] convertedItems = Array.ConvertAll<string, int>(values, int.Parse);
                     triangle.Add(new List<int>(convertedItems));
                 };
-            
              
             return triangle;
         }
